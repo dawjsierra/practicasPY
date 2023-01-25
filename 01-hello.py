7d9351e869f6426b2aaa230024163e16b10962cf
dawjsierra <sierra.javier.daw2@gmail.com>
2023-01-25 23:27:51 +0100

    primeros pasos py

---
+++ 01-hello.py
@@ -0,0 +1,15 @@
+#esto es un comentario
+
+# hola mundo en py
+print("Hola Python")
+
+"""
+ESTE ES 
+UN COMENTARIO EN 
+VARIAS LINEAS
+"""
+
+
+# Consultar el tipo de dato
+print(type(1)) #tipo str
+print(type(3 + 1j)) #tipo complex
